fa364d4463281dadf27160579122452e5e19c0f6
shubham singh rathore <shubhamsingh5169@gmail.com>
2024-03-24 18:24:28 +0530

    👋

---
+++ script.js
@@ -0,0 +1,11 @@
+//Game of Guessing Correct Number
+
+let gameNo = 30;
+let userNo = prompt("Enter the correct Number: ");
+
+while(gameNo != userNo){
+userNo = prompt("Sorry You Enter Wrong No. Try Again :");
+}
+
+console.log("Congratulation 👋 You Entered the Correct Number");
+                        
